2b26bf5ac44f7a0a69690f54cf20775662ef9b87
Giulia Cajati <giuliacajati@gmail.com>
2020-06-22 20:20:25 +0000

    Done.

--- lib / iteration_with_loops.rb
+++ lib / iteration_with_loops.rb
@@ -5,14 +5,17 @@ while row_index < src.count do
   element_index = 0
   
   min_num = 0
-  
+
   while element_index < src[row_index].count do
+   
     if src[row_index][element_index].min
       min_num = src[row_index][element_index]
    outer_results<< min_num
+    
     end
     element_index += 1
   end
   row_index += 1
 end
+
 outer_results
