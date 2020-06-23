0edc9ae1f751b7ba9e03f16752261d4910205ce8
Giulia Cajati <giuliacajati@gmail.com>
2020-06-23 19:38:33 +0000

    Done.

--- lib / iteration_with_loops.rb
+++ lib / iteration_with_loops.rb
@@ -1,21 +1,44 @@
 def find_min_in_nested_arrays(src)
-outer_results = []
-row_index = 0
-while row_index < src.count do
-  element_index = 0
-  
-  min_num = 0
-
-  while element_index < src[row_index].count do
-   
-    if src[row_index][element_index].min
-      min_num = src[row_index][element_index]
-   outer_results<< min_num
-    
+  outer_results = []
+  row_index = 0
+  while row_index < src.count do
+    element_index = 0
+    inner_results = []
+    lowest_value = 100
+    while element_index < src[row_index].count do
+      if src[row_index][element_index] < lowest_value
+        lowest_value = src[row_index][element_index]
+      end
+      element_index += 1
     end
-    element_index += 1
+    outer_results << lowest_value
+    row_index += 1
   end
-  row_index += 1
+  outer_results
+
 end
 
-outer_results
+
+#def find_min_in_nested_arrays(src)
+  #min_array = []
+  
+  #row_index = 0
+  #while row_index < src.count do
+    #element_index = 0
+    #min_num = src[row_index][0]
+    
+   # while element_index < src[row_index].count do
+     # if src[row_index][element_index][i] < min_num 
+        min_num = src[row_index][element_index]
+    #  end
+     # element_index += 1
+    #  end
+   # min_array.push(min_num)
+    #row_index += 1
+  #end 
+   # min_array
+#end
+
+ 
+
+
